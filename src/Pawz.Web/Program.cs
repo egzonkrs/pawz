8af7249ad5b383b3f4715f526ee2a1bb31bb5348
Daorsa Hyseni <79546481+Daxrsa@users.noreply.github.com>
2024-08-01 13:08:45 +0200

    PAWZ-20-Configured entity framework, added db connection and tested migrations

--- src/Pawz.Web/Program.cs
+++ src/Pawz.Web/Program.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Pawz.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connectionString));
 
 var app = builder.Build();
 
