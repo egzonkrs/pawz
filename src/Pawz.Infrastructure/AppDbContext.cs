59cda8975703e2d379b2e39bbaa85dd4e35e64ff
lauramuzaqi <muzaqilaura@gmail.com>
2024-08-01 21:38:37 +0200

    PAWZ-8 : Inherited IdentityDbContext; changed constructor parameter.

--- src/Pawz.Infrastructure/AppDbContext.cs
+++ src/Pawz.Infrastructure/AppDbContext.cs
@@ -1,8 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Pawz.Infrastructure;
 
-public class AppDbContext(DbContextOptions options) : DbContext(options)
+public class AppDbContext : IdentityDbContext<IdentityUser>
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
 }
+//    (DbContextOptions options) : DbContext(options)
+//{
+
+//}
