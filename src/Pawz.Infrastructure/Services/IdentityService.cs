2e9b8fb9f157931027f16cf66bcc1015b05155e4
lauramuzaqi <muzaqilaura@gmail.com>
2024-08-05 11:50:46 +0200

    PAWZ-39 feat: Added a new line.

--- src/Pawz.Infrastructure/Services/IdentityService.cs
+++ src/Pawz.Infrastructure/Services/IdentityService.cs
@@ -2,6 +2,7 @@ using Pawz.Application.Interfaces;
 using System.Threading.Tasks;
 
 namespace Pawz.Infrastructure.Services;
+
 public class IdentityService : IIdentityService
 {
     public Task LoginAsync()
