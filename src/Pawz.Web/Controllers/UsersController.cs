61d9b9c1914b356a1841817c1dda804558c43c30
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-06 14:22:55 +0200

    PAWZ-40 feat: Implement the Register method inside the IdentityService interface and Added UserManager as service.

--- src/Pawz.Web/Controllers/UsersController.cs
+++ src/Pawz.Web/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Pawz.Application.Interfaces;
+using Pawz.Domain.Entities;
+using Pawz.Web.Models;
 using System.Threading.Tasks;
 
 namespace Pawz.Web.Controllers;
@@ -13,9 +15,34 @@ public class UsersController : Controller
     }
 
     [HttpPost("register")]
-    public async Task<IActionResult> Register()
+    public async Task<IActionResult> Register(RegisterVM model)
     {
-        return Ok();
+        if (ModelState.IsValid)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Address = model.Address
+            };
+
+            var result = await _identityService.RegisterAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+        }
+        return View(model);
     }
 
     [HttpPost("login")]
