4147806627ed093ba0c9581e7babdc2cca100770
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-06 14:31:11 +0200

    Reapply "PAWZ-40 feat: Implement the Register method inside the IdentityService interface and Added UserManager as service."
    This reverts commit 5a58e238b06556135eeccbf87b6187f1eb2a8a55.

--- src/Pawz.Infrastructure/Services/IdentityService.cs
+++ src/Pawz.Infrastructure/Services/IdentityService.cs
@@ -1,19 +1,35 @@
+using Microsoft.AspNetCore.Identity;
 using Pawz.Application.Interfaces;
+using Pawz.Domain.Entities;
+using System;
 using System.Threading.Tasks;
 
 namespace Pawz.Infrastructure.Services;
 
 public class IdentityService : IIdentityService
 {
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public IdentityService(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
     public Task LoginAsync()
     {
         //TODO:
         return Task.CompletedTask;
     }
 
-    public Task RegisterAsync()
+    public async Task<IdentityResult> RegisterAsync(ApplicationUser user, string password)
     {
-        //TODO:
-        return Task.CompletedTask;
+        if (user == null)
+            throw new ArgumentNullException(nameof(user), "User cannot be null!");
+
+        if (string.IsNullOrEmpty(password))
+            throw new ArgumentNullException(nameof(password), "Password cannot be null or empty!");
+
+        var result = await _userManager.CreateAsync(user, password);
+        return result;
     }
 }
