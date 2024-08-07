ec8fb9e802787a627ce5bd5ea20216811c109462
lauramuzaqi <muzaqilaura@gmail.com>
2024-08-07 16:29:00 +0200

    PAWZ-41: Merged branch PAWZ-40-implement-register-functionality to current branch. Implemented Login.

--- src/Pawz.Web/Controllers/UsersController.cs
+++ src/Pawz.Web/Controllers/UsersController.cs
@@ -14,16 +14,11 @@ public class UsersController : Controller
         _identityService = identityService;
     }
 
-    [HttpPost("register")]
+    [HttpPost]
     public async Task<IActionResult> Register(RegisterViewModel model)
     {
         if (!ModelState.IsValid)
         {
-            AddModelErrorIfEmpty(model.Email, nameof(model.Email), "Email is required.");
-            AddModelErrorIfEmpty(model.Password, nameof(model.Password), "Password is required.");
-            AddModelErrorIfEmpty(model.FirstName, nameof(model.FirstName), "First Name is required.");
-            AddModelErrorIfEmpty(model.LastName, nameof(model.LastName), "Last Name is required.");
-
             return View(model);
         }
 
@@ -42,31 +37,24 @@ public class UsersController : Controller
             return RedirectToAction("Index", "Home");
         }
 
-        foreach (var error in result.Errors)
-        {
-            ModelState.AddModelError(error.Code, error.Description);
-        }
-
         return View(model);
     }
 
-    [HttpPost("login")]
-    public async Task<IActionResult> Login()
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model)
     {
-        return Ok();
-    }
-
-    /// <summary>
-    /// Helper method to add model error if a field is empty or null
-    /// </summary>
-    /// <param name="fieldValue"></param>
-    /// <param name="fieldName"></param>
-    /// <param name="errorMessage"></param>
-    private void AddModelErrorIfEmpty(string fieldValue, string fieldName, string errorMessage)
-    {
-        if (string.IsNullOrWhiteSpace(fieldValue))
+        if (!ModelState.IsValid)
         {
-            ModelState.AddModelError(fieldName, errorMessage);
+            return View(model);
         }
+
+        var result = await _identityService.LoginAsync(model.Email, model.Password);
+
+        if (result.Succeeded)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View(model);
     }
 }
