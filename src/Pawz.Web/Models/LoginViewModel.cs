af5d525cdaa5b2b6469641ab9afcc702aaed6a56
lauramuzaqi <muzaqilaura@gmail.com>
2024-08-07 16:31:29 +0200

    PAWZ-41: Created LoginViewModel; added properties; added summaries.

---
+++ src/Pawz.Web/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+namespace Pawz.Web.Models;
+
+public class LoginViewModel
+{
+    /// <summary>
+    /// Gets or sets the email address of the user.
+    /// </summary>
+    public string Email { get; set; }
+
+    /// <summary>
+    /// Gets or sets the password of the user.
+    /// </summary>
+    public string Password { get; set; }
+}
