35ed6f7bd50daa5885715d1f2e645fa5612c4aa2
granitlekaj <gl47538@ubt-uni.net>
2024-08-07 11:10:40 +0200

    feat:PAWZ-51-Create-MVC-Home-Page-with-a-Header-and-Pet-List

--- src/Pawz.Web/Controllers/HomeController.cs
+++ src/Pawz.Web/Controllers/HomeController.cs
@@ -1,38 +1,27 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Pawz.Web.Models;
 
-namespace Pawz.Web.Controllers;
-
-public class HomeController : Controller
+namespace YourNamespace.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult Shop()
-    {
-        return View();
-    }
+        public IActionResult Shop()
+        {
+            return View();
+        }
 
-    public IActionResult Adopt()
-    {
-        return View();
-    }
+        public IActionResult Adopt()
+        {
+            return View();
+        }
 
-    public IActionResult About()
-    {
-        return View();
-    }
-    public IActionResult SignIn()
-    {
-        return View();
-    }
-
-    public IActionResult Register()
-    {
-        return View();
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
