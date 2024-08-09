6c7e03f59bf369d0fe721f7327e226d4b2f4a5fb
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-09 11:01:36 +0200

    PAWZ-40 feat: Added RegisterRequest as parameter to RegisterAsync method.

--- src/Pawz.Application/Interfaces/IIdentityService.cs
+++ src/Pawz.Application/Interfaces/IIdentityService.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Identity;
-using Pawz.Domain.Entities;
+using Pawz.Application.Models;
 using System.Threading.Tasks;
 
 namespace Pawz.Application.Interfaces;
@@ -10,7 +10,7 @@ public interface IIdentityService
     /// Registers a new user with the provided details.
     /// </summary>
     /// <returns>A task representing the asynchronous operation.</returns>
-    Task<IdentityResult> RegisterAsync(ApplicationUser user, string password);
+    Task<IdentityResult> RegisterAsync(RegisterRequest request);
 
     /// <summary>
     /// Logs in a user with the provided credentials.
