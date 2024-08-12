1f86e2afcc3d2fdb72c6069a0d5336caea3ab57c
granitlekaj <gl47538@ubt-uni.net>
2024-08-12 13:39:17 +0200

    feat/PAWZ-36-Install-FluentValidation-Library-And-Setup-It-Inside-the-Project

--- src/Pawz.Web/Program.cs
+++ src/Pawz.Web/Program.cs
@@ -6,24 +6,39 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Pawz.Application.Interfaces;
 using Pawz.Infrastructure;
+using Pawz.Domain.FluentValidation;
 using Pawz.Infrastructure.Services;
+using FluentValidation.AspNetCore;
+using FluentValidation;
+using Pawz.Web.Models;
+using Pawz.Domain.Entities; // Import the namespace where your validators are defined
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+// Add services to the container.
+builder.Services.AddControllersWithViews()
+    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<ApplicationUserValidation>());
 
+// Register Swagger services
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+// Register your DbContext
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(connectionString));
 
+// Register Identity
 builder.Services.AddIdentityCore<IdentityUser>()
                 .AddEntityFrameworkStores<AppDbContext>();
 
+// Register scoped services
 builder.Services.AddScoped<IIdentityService, IdentityService>();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -34,6 +49,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+// Enable Swagger middleware
+//app.UseSwagger();
+//app.UseSwaggerUI(c =>
+//{
+//    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+//    c.RoutePrefix = string.Empty; // Swagger UI at the root URL
+//});
 
 app.UseAuthorization();
 
