4edb7cbbde69e18314c7d377e695e085b64e21ba
lauramuzaqi <muzaqilaura@gmail.com>
2024-08-13 11:19:44 +0200

    PAWZ-49: Removed Asynchronous keyword.

--- src/Pawz.Domain/Interfaces/IAdoptionRequestRepository.cs
+++ src/Pawz.Domain/Interfaces/IAdoptionRequestRepository.cs
@@ -8,15 +8,15 @@ namespace Pawz.Domain.Interfaces;
 public interface IAdoptionRequestRepository : IGenericRepository<AdoptionRequest, int>
 {
     /// <summary>
-    /// Asynchronously retrieves adoption requests based on their status.
+    /// Retrieves adoption requests based on their status.
     /// </summary>
     /// <param name="status">The status of the adoption requests to retrieve.</param>
     /// <param name="cancellationToken">The cancellation token to observe.</param>
     /// <returns>A task that represents the asynchronous operation. The task result contains a collection of adoption requests with the specified status.</returns>
-    Task<IEnumerable<AdoptionRequest>> GetRequestsByStatusAsync(PetStatus status, CancellationToken cancellationToken = default);
+    Task<IEnumerable<AdoptionRequest>> GetRequestsByStatusAsync(AdoptionRequestStatus status, CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Asynchronously retrieves adoption requests based on the associated pet's ID.
+    /// Retrieves adoption requests based on the associated pet's ID.
     /// </summary>
     /// <param name="petId">The ID of the pet associated with the adoption requests to retrieve.</param>
     /// <param name="cancellationToken">The cancellation token to observe.</param>
@@ -24,7 +24,7 @@ public interface IAdoptionRequestRepository : IGenericRepository<AdoptionRequest
     Task<IEnumerable<AdoptionRequest>> GetByPetIdAsync(int petId, CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Asynchronously retrieves adoption requests made by a specific user.
+    /// Retrieves adoption requests made by a specific user.
     /// </summary>
     /// <param name="userId">The ID of the user who made the adoption requests.</param>
     /// <param name="cancellationToken">The cancellation token to observe.</param>
