581d0b2d581453c4c91d4994e4139c07453d86cd
lauramuzaqi <muzaqilaura@gmail.com>
2024-08-13 11:16:42 +0200

    PAWZ-49: Refactor AdoptionRequest entity to use AdoptionRequestStatus enum instead of string for the Status property.

--- src/Pawz.Domain/Entities/AdoptionRequest.cs
+++ src/Pawz.Domain/Entities/AdoptionRequest.cs
@@ -1,3 +1,4 @@
+using Pawz.Domain.Enum;
 using Pawz.Domain.Interfaces;
 using System;
 
@@ -18,7 +19,7 @@ public class AdoptionRequest : IEntity<int>
     /// <summary>
     /// The status of the adoption request
     /// </summary>
-    public string Status { get; set; }
+    public AdoptionRequestStatus Status { get; set; }
 
     /// <summary>
     /// The date when the adoption request was made
