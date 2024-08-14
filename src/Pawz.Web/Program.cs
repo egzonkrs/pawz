752625b0e78ae34081d749d7788eb67b1ba7f955
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-14 13:24:27 +0200

    Revert "PAWZ-58 feat: Created and implemented ModuleService"
    This reverts commit 6b68a47c9b32811a2149672b1d62809186ce6e1e.

--- src/Pawz.Web/Program.cs
+++ src/Pawz.Web/Program.cs
@@ -1,29 +1,39 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Pawz.Application.Extensions;
-using Pawz.Application.Services;
+using Pawz.Application.Interfaces;
 using Pawz.Domain.Entities;
+using Pawz.Domain.Interfaces;
 using Pawz.Infrastructure.Data;
-using Pawz.Infrastructure.Modules;
 using Pawz.Infrastructure.Repos;
+using Pawz.Infrastructure.Services;
 using System;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connectionString));
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-builder.Services.AddModule<DataModule>();
-builder.Services.AddModule<ModuleRepository>();
-builder.Services.AddModule<ModuleService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IPetRepository, PetRepository>();
+builder.Services.AddScoped<IAdoptionRequestRepository, AdoptionRequestRepository>();
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<AppDbContext>()
                 .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IIdentityService, IdentityService>();
 
 var app = builder.Build();
 
