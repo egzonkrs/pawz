1aa1623b1fd2805aeba9ce064841af82f3993e7b
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-16 01:58:39 +0200

    PAWZ-65 feat: Updated IIdentityservice to use Result pattern for RegisterAsync method.

--- src/Pawz.Application/Interfaces/IIdentityService.cs
+++ src/Pawz.Application/Interfaces/IIdentityService.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Pawz.Application.Models;
+using Pawz.Domain.Common;
 using System.Threading.Tasks;
 
 namespace Pawz.Application.Interfaces;
@@ -10,7 +11,7 @@ public interface IIdentityService
     /// Registers a new user with the provided details.
     /// </summary>
     /// <returns>A task representing the asynchronous operation.</returns>
-    Task<IdentityResult> RegisterAsync(RegisterRequest request);
+    Task<Result<IdentityResult>> RegisterAsync(RegisterRequest request);
 
     /// <summary>
     /// Logs in a user with the provided credentials.
