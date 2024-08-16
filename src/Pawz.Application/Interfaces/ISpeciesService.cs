5138c588e96be28e01d83ac0eb175c21e2b2e14f
lauramuzaqi <muzaqilaura@gmail.com>
2024-08-16 08:43:13 +0200

    PAWZ-71 feat: Add ISpeciesService interface with CRUD methods for Species entity

---
+++ src/Pawz.Application/Interfaces/ISpeciesService.cs
@@ -0,0 +1,49 @@
+using Pawz.Domain.Common;
+using Pawz.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pawz.Application.Interfaces;
+
+public interface ISpeciesService
+{
+    /// <summary>
+    /// Creates a new species.
+    /// </summary>
+    /// <param name="species">The species entity to create.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+    /// <returns>A task representing the operation. The task result contains a boolean indicating success or failure.</returns>
+    Task<Result<bool>> CreateSpeciesAsync(Species species, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Retrieves all species.
+    /// </summary>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+    /// <returns>A task representing the operation. The task result contains a collection of species entities.</returns>
+    Task<Result<IEnumerable<Species>>> GetAllSpeciesAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Retrieves a species by its ID.
+    /// </summary>
+    /// <param name="speciesId">The ID of the species to retrieve.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+    /// <returns>A task representing the operation. The task result contains the species entity.</returns>
+    Task<Result<Species>> GetSpeciesByIdAsync(int speciesId, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Updates an existing species.
+    /// </summary>
+    /// <param name="species">The species entity to update.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+    /// <returns>A task representing the operation. The task result contains a boolean indicating success or failure.</returns>
+    Task<Result<bool>> UpdateSpeciesAsync(Species species, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Deletes a species by its ID.
+    /// </summary>
+    /// <param name="speciesId">The ID of the species to delete.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+    /// <returns>A task representing the operation. The task result contains a boolean indicating success or failure.</returns>
+    Task<Result<bool>> DeleteSpeciesAsync(int speciesId, CancellationToken cancellationToken);
+}
