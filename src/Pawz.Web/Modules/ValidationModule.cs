0338e363339c92fa07029d00e00f29868623cc67
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-19 09:45:08 +0200

    PAWZ-44 feat: Added ValidationModule to register FluentValidation validators for RegisterViewModel.

---
+++ src/Pawz.Web/Modules/ValidationModule.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Pawz.Domain.Abstractions;
+using Pawz.Web.Models;
+using Pawz.Web.Validators;
+
+namespace Pawz.Web.Modules;
+
+public class ValidationModule : IModule
+{
+    public void Load(IServiceCollection services)
+    {
+        services.AddValidatorsFromAssemblyContaining<RegisterViewModel>();
+        services.AddScoped<IValidator<RegisterViewModel>, RegisterViewModelValidator>();
+    }
+}
