d552c7152b2985d6fabedc756d0e2c0a97d74956
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-19 09:42:59 +0200

    PAWZ-44 feat: Added extension methods to ValidationExtensions for adding FluentValidation and Result errors to ModelState.

--- src/Pawz.Web/Extensions/ValidationExtensions.cs
+++ src/Pawz.Web/Extensions/ValidationExtensions.cs
@@ -1,15 +1,16 @@
-using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Pawz.Domain.Common;
 
 namespace Pawz.Web.Extensions;
 
 public static class ValidationExtensions
 {
-    public static void AddToModelState(this ValidationResult result, ModelStateDictionary modelState)
+
+    public static void AddToModelState(this Result<bool> result, ModelStateDictionary modelState)
     {
         foreach (var error in result.Errors)
         {
-            modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+            modelState.AddModelError(string.Empty, error.Description);
         }
     }
 }
