7bbe919ffd78dd5642932a5a7a10c74b807552d1
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-19 12:12:51 +0200

    PAWZ-44 feat: renamed method AddModelState() to AddValidationErrorsToModelState().

--- src/Pawz.Web/Extensions/ValidationExtensions.cs
+++ src/Pawz.Web/Extensions/ValidationExtensions.cs
@@ -1,3 +1,4 @@
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Pawz.Domain.Common;
 
@@ -5,12 +6,19 @@ namespace Pawz.Web.Extensions;
 
 public static class ValidationExtensions
 {
+    public static void AddValidationErrorsToModelState(this ValidationResult result, ModelStateDictionary modelState)
+    {
+        foreach (var error in result.Errors)
+        {
+            modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+        }
+    }
 
     public static void AddToModelState(this Result<bool> result, ModelStateDictionary modelState)
     {
         foreach (var error in result.Errors)
         {
-            modelState.AddModelError(string.Empty, error.Description);
+            modelState.AddModelError(error.Code, error.Description);
         }
     }
 }
