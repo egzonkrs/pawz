c035c6316bbf514e11ceb9d02882c05fdafde79d
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-19 09:44:22 +0200

    PAWZ-44 feat: Removed DataAnnotations from RegisterViewModel and used init-only properties.

--- src/Pawz.Web/Models/RegisterViewModel.cs
+++ src/Pawz.Web/Models/RegisterViewModel.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Pawz.Web.Models;
 
 public class RegisterViewModel
@@ -8,38 +6,29 @@ public class RegisterViewModel
     /// Gets or sets the email address of the user.
     /// This is required and must be a valid email address.
     /// </summary>
-    [Required(ErrorMessage = "Email is required.")]
-    [EmailAddress(ErrorMessage = "Invalid email address.")]
-    public string Email { get; set; }
+    public string Email { get; init; }
 
     /// <summary>
     /// Gets or sets the password for the user.
     /// This is required and should be a secure password.
     /// </summary>
-    [Required(ErrorMessage = "Password is required.")]
-    [DataType(DataType.Password, ErrorMessage = "Invalid password format.")]
-    public string Password { get; set; }
+    public string Password { get; init; }
 
     /// <summary>
     /// Gets or sets the confirmation password.
     /// This is required and must match the password.
     /// </summary>
-    [Required(ErrorMessage = "Confirm Password is required.")]
-    [DataType(DataType.Password, ErrorMessage = "Invalid password format.")]
-    [Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
-    public string ConfirmPassword { get; set; }
+    public string ConfirmPassword { get; init; }
 
     /// <summary>
     /// Gets or sets the first name of the user.
     /// This is required.
     /// </summary>
-    [Required(ErrorMessage = "First Name is required.")]
-    public string FirstName { get; set; }
+    public string FirstName { get; init; }
 
     /// <summary>
     /// Gets or sets the last name of the user.
     /// This is required.
     /// </summary>
-    [Required(ErrorMessage = "Last Name is required.")]
-    public string LastName { get; set; }
+    public string LastName { get; init; }
 }
