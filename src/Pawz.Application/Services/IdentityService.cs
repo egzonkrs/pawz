537bca4469bb0e94c05a33ad9b9ba43b01e47bcb
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-19 09:40:26 +0200

    PAWZ-44 feat: Updated IdentityService with logging and detailed error handling for user registration.

--- src/Pawz.Application/Services/IdentityService.cs
+++ src/Pawz.Application/Services/IdentityService.cs
@@ -1,24 +1,28 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 using Pawz.Application.Interfaces;
 using Pawz.Application.Models;
 using Pawz.Domain.Common;
 using Pawz.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Pawz.Application.Services;
 
-public class IdentityService : IIdentityService
+public sealed class IdentityService : IIdentityService
 {
+    private readonly ILogger<IIdentityService> _logger;
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
 
-    public IdentityService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    public IdentityService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ILogger<IIdentityService> logger)
     {
         _userManager = userManager;
         _signInManager = signInManager;
+        _logger = logger;
     }
 
     public async Task<SignInResult> LoginAsync(LoginRequest request)
@@ -45,9 +49,11 @@ public class IdentityService : IIdentityService
                 CreatedAt = DateTime.UtcNow,
             };
 
+            _logger.LogInformation($"Started registering User with Email: {request.Email} and CreatedAt: {user.CreatedAt}");
+
             var result = await _userManager.CreateAsync(user, request.Password);
 
-            if (!result.Succeeded)
+            if (result.Succeeded is false)
             {
                 return Result<bool>.Failure(UserErrors.CreationFailed);
             }
@@ -62,10 +68,19 @@ public class IdentityService : IIdentityService
 
             var claimResult = await _userManager.AddClaimsAsync(user, userClaims);
 
-            return claimResult.Succeeded ? Result<bool>.Success() : Result<bool>.Failure(UserErrors.ClaimFailed);
+            if (claimResult.Succeeded is false)
+            {
+                _logger.LogError($"Failed to create user claims for the User with Email: {user.Email} " +
+                                 $"with Errors: {string.Join(",", claimResult.Errors.Select(x => x.Description))}");
+
+                return Result<bool>.Failure(UserErrors.ClaimFailed);
+            }
+
+            return Result<bool>.Success();
         }
-        catch (Exception)
+        catch (Exception ex)
         {
+            _logger.LogError(ex, $"An error occured in the {nameof(IdentityService)} while attempting to register User with Email: {request.Email}");
             return Result<bool>.Failure(UserErrors.UnexpectedError);
         }
     }
