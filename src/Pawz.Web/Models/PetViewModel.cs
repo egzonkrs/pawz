243d78482b7b8715dce38ec64d6f4a9fdc4845ac
Daorsa Hyseni <79546481+Daxrsa@users.noreply.github.com>
2024-08-20 13:19:16 +0200

    PAWZ-51: Added PetViewModel

---
+++ src/Pawz.Web/Models/PetViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class PetViewModel
+{
+    public string Name { get; set; }
+    public string About { get; set; }
+    public string Species { get; set; }
+    public string Breed { get; set; }
+    public int AgeYears { get; set; }
+    public int AgeMonths { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+}
