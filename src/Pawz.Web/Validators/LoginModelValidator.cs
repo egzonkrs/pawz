3c693728673cd03a8a73008715c22d34cfaa5102
shuajbkingu <shuajbmemaa@hotmail.com>
2024-08-20 13:08:13 +0200

    PAWZ-45 : Modified LoginModel Validator and improved error display in view.

--- src/Pawz.Web/Validators/LoginModelValidator.cs
+++ src/Pawz.Web/Validators/LoginModelValidator.cs
@@ -8,8 +8,10 @@ public class LoginModelValidator : AbstractValidator<LoginViewModel>
     public LoginModelValidator()
     {
         RuleFor(x => x.Email)
-           .NotEmpty().WithMessage("Email is required.")
-           .EmailAddress().WithMessage("A valid email address is required.");
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Email is required")
+            .EmailAddress().WithMessage("A valid email address is required.");
 
         RuleFor(x => x.Password)
             .NotEmpty().WithMessage("Password is required.");
