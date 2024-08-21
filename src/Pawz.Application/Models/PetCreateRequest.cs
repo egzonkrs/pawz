fc6128ca98cd3ca7dc6d2b4cffe39f083c00c1fd
lauramuzaqi <muzaqilaura@gmail.com>
2024-08-21 11:20:41 +0200

    PAWZ-68 feat: Created PetCreateRequest

---
+++ src/Pawz.Application/Models/PetCreateRequest.cs
@@ -0,0 +1,61 @@
+using Pawz.Domain.Enums;
+using Pawz.Domain.Interfaces;
+
+namespace Pawz.Application.Models;
+
+public class PetCreateRequest : IEntity<int>
+{
+    /// <summary>
+    /// The Id of the pet
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// The name of the pet.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The Id of the species of the pet.
+    /// </summary>
+    public int SpeciesId { get; set; }
+
+    /// <summary>
+    /// The Id of the breed of the pet.
+    /// </summary>
+    public int BreedId { get; set; }
+
+    /// <summary>
+    /// The age of the pet in years.
+    /// </summary>
+    public int AgeYears { get; set; }
+
+    /// <summary>
+    /// The age of the pet in months.
+    /// </summary>
+    public int AgeMonths { get; set; }
+
+    /// <summary>
+    /// Additional information about the pet.
+    /// </summary>
+    public string About { get; set; }
+
+    /// <summary>
+    /// The price of the pet.
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// The Id of the location where the pet is located.
+    /// </summary>
+    public int LocationId { get; set; }
+
+    /// <summary>
+    /// The ID of the user who posted the pet.
+    /// </summary>
+    public string PostedByUserId { get; set; }
+
+    /// <summary>
+    /// Status of the pet (e.g., 'Pending', 'Approved', 'Rejected').
+    /// </summary>
+    public PetStatus Status { get; set; }
+}
