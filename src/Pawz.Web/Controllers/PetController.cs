8da344dde830a537f35e8e09cc1f614df5ea19a8
lauramuzaqi <muzaqilaura@gmail.com>
2024-08-21 11:46:48 +0200

    PAWZ-69 feat: Implement Create action methods to create a pet.

--- src/Pawz.Web/Controllers/PetController.cs
+++ src/Pawz.Web/Controllers/PetController.cs
@@ -1,17 +1,33 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Pawz.Application.Interfaces;
+using Pawz.Application.Models;
 using Pawz.Domain.Entities;
+using Pawz.Web.Extensions;
+using Pawz.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Pawz.Web.Controllers
 {
+    [Authorize]
     public class PetController : Controller
     {
         private readonly IPetService _petService;
-        public PetController(IPetService petService)
+        private readonly ISpeciesService _speciesService;
+        private readonly IBreedService _breedService;
+        private readonly IValidator<PetCreateViewModel> _validator;
+        public PetController(IPetService petService, ISpeciesService speciesService, IBreedService breedService, IValidator<PetCreateViewModel> validator)
         {
             _petService = petService;
+            _speciesService = speciesService;
+            _breedService = breedService;
+            _validator = validator;
         }
 
         public async Task<IActionResult> Index(CancellationToken cancellationToken)
@@ -26,17 +42,56 @@ namespace Pawz.Web.Controllers
             return View(pet);
         }
 
-        public IActionResult Create()
+        [Authorize]
+        public async Task<IActionResult> Create(CancellationToken cancellationToken)
         {
-            return View();
+            var petCreateViewModel = new PetCreateViewModel
+            {
+                Species = await GetSpeciesSelectListItemsAsync(cancellationToken),
+                Breeds = await GetBreedsSelectListItemsAsync(cancellationToken)
+            };
+
+            return View(petCreateViewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Pet pet, CancellationToken cancellationToken)
+        public async Task<IActionResult> Create(PetCreateViewModel petCreateViewModel, CancellationToken cancellationToken)
         {
-            await _petService.CreatePetAsync(pet, cancellationToken);
-            return RedirectToAction(nameof(Index));
+            petCreateViewModel.Species = await GetSpeciesSelectListItemsAsync(cancellationToken);
+            petCreateViewModel.Breeds = await GetBreedsSelectListItemsAsync(cancellationToken);
+
+            var validationResult = await _validator.ValidateAsync(petCreateViewModel);
+
+            if (validationResult.IsValid is false)
+            {
+                validationResult.AddErrorsToModelState(ModelState);
+                return View(petCreateViewModel);
+            }
+
+            var petCreateRequest = new PetCreateRequest
+            {
+                Name = petCreateViewModel.Name,
+                SpeciesId = petCreateViewModel.SpeciesId,
+                BreedId = petCreateViewModel.BreedId,
+                AgeYears = petCreateViewModel.AgeYears,
+                AgeMonths = petCreateViewModel.AgeMonths,
+                About = petCreateViewModel.About,
+                Price = petCreateViewModel.Price,
+                LocationId = petCreateViewModel.LocationId,
+                Status = petCreateViewModel.Status,
+                PostedByUserId = User.FindFirstValue(ClaimTypes.NameIdentifier)
+            };
+
+            var petCreateResult = await _petService.CreatePetAsync(petCreateRequest, cancellationToken);
+
+            if (petCreateResult.IsSuccess is false)
+            {
+                petCreateResult.AddErrorsToModelState(ModelState);
+                return View(petCreateViewModel);
+            }
+
+            return RedirectToAction("Index", "Home");
         }
 
         public async Task<IActionResult> Edit(int id, CancellationToken cancellationToken)
@@ -66,5 +121,35 @@ namespace Pawz.Web.Controllers
             await _petService.DeletePetAsync(id, cancellationToken);
             return RedirectToAction(nameof(Index));
         }
+
+        private async Task<IEnumerable<SelectListItem>> GetSpeciesSelectListItemsAsync(CancellationToken cancellationToken)
+        {
+            var result = await _speciesService.GetAllSpeciesAsync(cancellationToken);
+            if (result.IsSuccess)
+            {
+                return result.Value.Select(species => new SelectListItem
+                {
+                    Text = species.Name,
+                    Value = species.Id.ToString(),
+                });
+            }
+            return Enumerable.Empty<SelectListItem>();
+        }
+
+        private async Task<IEnumerable<SelectListItem>> GetBreedsSelectListItemsAsync(CancellationToken cancellationToken)
+        {
+            var result = await _breedService.GetAllBreedsAsync(cancellationToken);
+            if (result.IsSuccess)
+            {
+                return result.Value.Select(breed => new SelectListItem
+                {
+                    Value = breed.Id.ToString(),
+                    Text = breed.Name
+                });
+            }
+            return Enumerable.Empty<SelectListItem>();
+        }
+
+
     }
 }
