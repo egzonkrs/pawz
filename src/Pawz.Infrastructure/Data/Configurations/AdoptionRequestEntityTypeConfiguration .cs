257d26ef07acc9656bb8c5afaa98c6ec50dc3632
lauramuzaqi <muzaqilaura@gmail.com>
2024-08-22 20:51:59 +0200

    PAWZ-68 feat: Added DeleteBehavior.Restrict to AdoptionRequest entity configuration to prevent cascading deletions when a Pet is deleted.

--- src/Pawz.Infrastructure/Data/Configurations/AdoptionRequestEntityTypeConfiguration .cs
+++ src/Pawz.Infrastructure/Data/Configurations/AdoptionRequestEntityTypeConfiguration .cs
@@ -1,16 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Pawz.Domain.Entities;
-using System;
 
 namespace Pawz.Infrastructure.Data.Configurations;
 
 public class AdoptionRequestEntityTypeConfiguration : IEntityTypeConfiguration<AdoptionRequest>
 {
-    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<AdoptionRequest> builder)
+    public void Configure(EntityTypeBuilder<AdoptionRequest> builder)
     {
         builder
             .HasOne(ar => ar.Pet)
             .WithMany(p => p.AdoptionRequests)
-            .HasForeignKey(ar => ar.PetId);
+            .HasForeignKey(ar => ar.PetId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
