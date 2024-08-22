c5438b1321fc2da8f9441ed2e0ba763251cb4cd6
lauramuzaqi <muzaqilaura@gmail.com>
2024-08-22 20:57:25 +0200

    PAWZ-68 feat: Updated locations seed data for SQL Server migration, adding IsDeleted and DeletedAt fields to align with the new database schema.

--- src/Pawz.Infrastructure/Data/Seed/SeedLocations.cs
+++ src/Pawz.Infrastructure/Data/Seed/SeedLocations.cs
@@ -7,16 +7,16 @@ public class SeedLocations
 {
     public static async Task SeedLocationData(AppDbContext context)
     {
-        var locationsExists = await context.Breeds.AnyAsync();
+        var locationsExists = await context.Locations.AnyAsync();
         if (locationsExists) return;
 
         await context.Database.ExecuteSqlRawAsync(@"
-                    INSERT INTO Locations (Id, City, State, Country, PostalCode) VALUES
-                    (1, 'New York', 'NY', 'USA', '10001'),
-                    (2, 'Los Angeles', 'CA', 'USA', '90001'),
-                    (3, 'Chicago', 'IL', 'USA', '60601'),
-                    (4, 'Houston', 'TX', 'USA', '77001'),
-                    (5, 'Toronto', 'ON', 'Canada', 'M5H 2N2')"
+            INSERT INTO Locations (City, State, Country, PostalCode, IsDeleted, DeletedAt) VALUES
+            ('New York', 'NY', 'USA', '10001', 0, NULL),
+            ('Los Angeles', 'CA', 'USA', '90001', 0, NULL),
+            ('Chicago', 'IL', 'USA', '60601', 0, NULL),
+            ('Houston', 'TX', 'USA', '77001', 0, NULL),
+            ('Toronto', 'ON', 'Canada', 'M5H 2N2', 0, NULL)"
         );
     }
 }
