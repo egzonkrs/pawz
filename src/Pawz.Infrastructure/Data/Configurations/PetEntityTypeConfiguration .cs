afc26d0009c75fae9505dae57c4f99489b600aee
lauramuzaqi <muzaqilaura@gmail.com>
2024-08-26 13:10:51 +0200

    PAWZ-68 fix: Remove Species relationship and OnDelete configuration from Pet entity.

--- src/Pawz.Infrastructure/Data/Configurations/PetEntityTypeConfiguration .cs
+++ src/Pawz.Infrastructure/Data/Configurations/PetEntityTypeConfiguration .cs
@@ -9,22 +9,14 @@ public class PetEntityTypeConfiguration : IEntityTypeConfiguration<Pet>
     public void Configure(EntityTypeBuilder<Pet> builder)
     {
         builder
-            .HasOne(p => p.Species)
-            .WithMany(s => s.Pets)
-            .HasForeignKey(p => p.SpeciesId)
-            .OnDelete(DeleteBehavior.NoAction);
-
-        builder
             .HasOne(p => p.Breed)
             .WithMany(b => b.Pets)
-            .HasForeignKey(p => p.BreedId)
-            .OnDelete(DeleteBehavior.NoAction);
+            .HasForeignKey(p => p.BreedId);
 
         builder
             .HasOne(p => p.Location)
             .WithMany(l => l.Pets)
-            .HasForeignKey(p => p.LocationId)
-            .OnDelete(DeleteBehavior.NoAction);
+            .HasForeignKey(p => p.LocationId);
 
         builder
             .Property(p => p.Price)
