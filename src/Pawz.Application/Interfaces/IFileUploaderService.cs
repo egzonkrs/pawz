d4e1ff16d0c36c9a64e13af1452858c4dd3c3742
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-27 14:02:30 +0200

    PAWZ-66: Added IFileUploaderService interface for uploading files.

---
+++ src/Pawz.Application/Interfaces/IFileUploaderService.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Pawz.Application.Interfaces;
+
+/// <summary>
+/// Interface for a service that handles file uploads.
+/// </summary>
+public interface IFileUploaderService
+{
+    /// <summary>
+    /// Uploads a file to a specified directory and returns the relative path of the uploaded file.
+    /// </summary>
+    /// <param name="file">The file to be uploaded.</param>
+    /// <param name="directory">The directory where the file will be saved.</param>
+    /// <returns>The relative path of the uploaded file.</returns>
+    Task<string> UploadFileAsync(IFormFile file, string directory);
+}
