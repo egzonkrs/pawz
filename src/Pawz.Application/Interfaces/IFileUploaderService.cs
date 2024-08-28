da49fdcb1165e1431ea61dfb62062a67e56775b5
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-28 14:35:33 +0200

    PAWZ-66 feat: Refactor IFileUploaderService to return Result<string> instead of string.

--- src/Pawz.Application/Interfaces/IFileUploaderService.cs
+++ src/Pawz.Application/Interfaces/IFileUploaderService.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http;
+using Pawz.Domain.Common;
 using System.Threading.Tasks;
 
 namespace Pawz.Application.Interfaces;
@@ -14,5 +15,5 @@ public interface IFileUploaderService
     /// <param name="file">The file to be uploaded.</param>
     /// <param name="directory">The directory where the file will be saved.</param>
     /// <returns>The relative path of the uploaded file.</returns>
-    Task<string> UploadFileAsync(IFormFile file, string directory);
+    Task<Result<string>> UploadFileAsync(IFormFile file, string directory);
 }
