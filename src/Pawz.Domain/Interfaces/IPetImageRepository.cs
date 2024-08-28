9c65ec1a0aa473d92b03dbdd3b09e53bb165bb6b
ruhsulin <alinaspahiu.a@gmail.com>
2024-08-28 14:36:49 +0200

    PAWZ-66 feat: Removed InsertAsync method since we are using it from Generic repositoy.

--- src/Pawz.Domain/Interfaces/IPetImageRepository.cs
+++ src/Pawz.Domain/Interfaces/IPetImageRepository.cs
@@ -1,19 +1,9 @@
 using Pawz.Domain.Entities;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Pawz.Domain.Interfaces;
 
 /// <summary>
 /// Interface for the repository that manages pet images.
 /// </summary>
-public interface IPetImageRepository : IGenericRepository<PetImage, int>
-{
-    /// <summary>
-    /// Inserts a new pet image into the repository asynchronously.
-    /// </summary>
-    /// <param name="petImage">The pet image entity to be inserted.</param>
-    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
-    /// <returns>The ID of the inserted pet image.</returns>
-    Task<int> InsertAsync(PetImage petImage, CancellationToken cancellationToken);
-}
+public interface IPetImageRepository : IGenericRepository<PetImage, int> { }
+
