a8f7835025e2d62ca20d266cdf916a3f289ed0ad
lauramuzaqi <muzaqilaura@gmail.com>
2024-09-01 13:44:41 +0200

    PAWZ-69 feat: Commented a rule in pet create validator.

--- src/Pawz.Web/Validators/PetCreateViewModelValidator.cs
+++ src/Pawz.Web/Validators/PetCreateViewModelValidator.cs
@@ -8,46 +8,41 @@ public class PetCreateViewModelValidator : AbstractValidator<PetCreateViewModel>
     public PetCreateViewModelValidator()
     {
         RuleFor(x => x.Name)
-            .NotNull().WithMessage("Name cannot be null.")
+            .NotNull()
             .NotEmpty().WithMessage("Name is required.")
             .MaximumLength(50).WithMessage("Name must be 50 characters or fewer.");
 
-        RuleFor(x => x.SpeciesId)
-            .NotNull().WithMessage("Species cannot be null.")
-            .NotEmpty().WithMessage("Species is required.")
-            .GreaterThan(0).WithMessage("Species must be selected.");
-
         RuleFor(x => x.BreedId)
-            .NotNull().WithMessage("Breed cannot be null.")
+            .NotNull()
             .NotEmpty().WithMessage("Breed is required.")
             .GreaterThan(0).WithMessage("Breed must be selected.");
 
         RuleFor(x => x.AgeYears)
-            .NotNull().WithMessage("Age in years cannot be null.")
+            .NotNull().WithMessage("Age in years is required.")
             .GreaterThanOrEqualTo(0).WithMessage("Age in years must be 0 or greater.")
             .LessThanOrEqualTo(20).WithMessage("Age in years must be 20 or fewer.");
 
         RuleFor(x => x.AgeMonths)
-            .NotNull().WithMessage("Age in months cannot be null.")
+            .NotNull().WithMessage("Age in months is required.")
             .GreaterThanOrEqualTo(0).WithMessage("Age in months must be 0 or greater.")
             .LessThan(12).WithMessage("Age in months must be less than 12.");
 
         RuleFor(x => x.About)
-            .NotNull().WithMessage("About section cannot be null.")
+            .NotNull()
             .NotEmpty().WithMessage("About section is required.")
             .MaximumLength(500).WithMessage("About section must be 500 characters or fewer.");
 
         RuleFor(x => x.Price)
-            .NotNull().WithMessage("Price cannot be null.")
+            .NotNull().WithMessage("Price is required.")
             .GreaterThanOrEqualTo(0).WithMessage("Price must be a positive value.");
 
-        RuleFor(x => x.LocationId)
-            .NotNull().WithMessage("Location cannot be null.")
-            .NotEmpty().WithMessage("Location is required.")
-            .GreaterThan(0).WithMessage("Location must be selected.");
+        //RuleFor(x => x.LocationId)
+        //    .NotNull()
+        //    .NotEmpty().WithMessage("Location is required.")
+        //    .GreaterThan(0).WithMessage("Location must be selected.");
 
         RuleFor(x => x.Status)
-            .NotNull().WithMessage("Status cannot be null.")
+            .NotNull().WithMessage("Status is required.")
             .IsInEnum().WithMessage("A valid pet status is required.");
     }
 }
