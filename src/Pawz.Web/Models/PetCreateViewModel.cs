e70130c3859a2ff2abbc1c5414c8da42aec1e7fb
lauramuzaqi <muzaqilaura@gmail.com>
2024-09-01 13:45:18 +0200

    PAWZ-69 fix: Changed PetCreateViewModel properties

--- src/Pawz.Web/Models/PetCreateViewModel.cs
+++ src/Pawz.Web/Models/PetCreateViewModel.cs
@@ -1,21 +1,41 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Pawz.Domain.Enums;
-using System.Collections.Generic;
 
 namespace Pawz.Web.Models;
 
 public class PetCreateViewModel
 {
+    public int Id { get; set; }
+
     public string Name { get; set; }
-    public int SpeciesId { get; set; }
-    public IEnumerable<SelectListItem>? Species { get; set; }
+
     public int BreedId { get; set; }
-    public IEnumerable<SelectListItem>? Breeds { get; set; }
+
+    public int SpeciesId { get; set; }
+
     public int AgeYears { get; set; }
+
     public int AgeMonths { get; set; }
+
     public string About { get; set; }
+
     public decimal Price { get; set; }
-    public int LocationId { get; set; }
-    public IEnumerable<SelectListItem>? Locations { get; set; }
+
     public PetStatus Status { get; set; }
+
+    public int LocationId { get; set; }
+
+    public int CityId { get; set; }
+
+    public int CountryId { get; set; }
+
+    public string Address { get; set; }
+
+    public string PostalCode { get; set; }
+
+    public SelectList Breeds { get; set; }
+    public SelectList Species { get; set; }
+    public SelectList Countries { get; set; }
+    public SelectList Cities { get; set; }
+    public SelectList Locations { get; set; }
 }
