f5e282b381c1fe2166e1793d4dc196d297b0c1ca
lauramuzaqi <muzaqilaura@gmail.com>
2024-09-02 19:50:13 +0200

    PAWZ-69 fix: Deleted unneccessary methods

--- src/Pawz.Domain/Interfaces/IBreedRepository.cs
+++ src/Pawz.Domain/Interfaces/IBreedRepository.cs
@@ -1,11 +1,5 @@
 using Pawz.Domain.Entities;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Pawz.Domain.Interfaces;
 
-public interface IBreedRepository : IGenericRepository<Breed, int>
-{
-    Task<IEnumerable<Breed>> GetBreedsBySpeciesIdAsync(int speciesId, CancellationToken cancellationToken);
-}
+public interface IBreedRepository : IGenericRepository<Breed, int> { }
