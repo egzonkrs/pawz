e0f5fdb293ecd6153242851286b55375356e160b
Daorsa Hyseni <79546481+Daxrsa@users.noreply.github.com>
2024-09-03 13:22:12 +0200

    PAWZ-51: chore

--- src/Pawz.Web/Program.cs
+++ src/Pawz.Web/Program.cs
@@ -42,21 +42,4 @@ app.UseAuthorization();
 
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-
-try
-{
-    var context = services.GetRequiredService<AppDbContext>();
-    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-    var unitOfWork = services.GetRequiredService<IUnitOfWork>();
-    await context.Database.MigrateAsync();
-    await DataSeeder.SeedData(context, unitOfWork, userManager);
-}
-catch (Exception ex)
-{
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error occured during migration");
-}
-
 app.Run();
