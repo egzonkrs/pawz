3383d3534a645852f97ff2956431a6aaa0490407
lauramuzaqi <muzaqilaura@gmail.com>
2024-09-03 17:28:18 +0200

    PAWZ-70 feat: Integrate Toastr Notifications

--- src/Pawz.Web/Controllers/PetController.cs
+++ src/Pawz.Web/Controllers/PetController.cs
@@ -131,6 +131,7 @@ public class PetController : Controller
             }).ToList();
 
             validationResult.AddErrorsToModelState(ModelState);
+            TempData["ErrorMessage"] = "Failed to create the pet! Please try again!";
             return View(petCreateViewModel);
         }
 
@@ -165,10 +166,10 @@ public class PetController : Controller
             return View(petCreateViewModel);
         }
 
-        return RedirectToAction("Index", "Home");
-
-
+        TempData["SuccessMessage"] = "Pet created successfully!";
+        return RedirectToAction("Details", "Pet");
     }
+
     public async Task<IActionResult> Edit(int id, CancellationToken cancellationToken)
     {
         var pet = await _petService.GetPetByIdAsync(id, cancellationToken);
