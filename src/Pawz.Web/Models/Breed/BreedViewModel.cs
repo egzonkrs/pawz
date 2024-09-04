571197ec5227fd5a25d0b71eaf45bb5f799dd3b4
Daorsa Hyseni <79546481+Daxrsa@users.noreply.github.com>
2024-09-04 18:54:09 +0200

    PAWZ-96: Created categories

--- src/Pawz.Web/Models/Breed/BreedViewModel.cs
+++ src/Pawz.Web/Models/Breed/BreedViewModel.cs
@@ -4,25 +4,8 @@ namespace Pawz.Web.Models.Breed;
 
 public class BreedViewModel
 {
-    /// <summary>
-    /// The Id of the breed
-    /// </summary>
     public int Id { get; set; }
-
-    /// <summary>
-    /// The name of the breed.
-    /// </summary>
     public string Name { get; set; }
-
-    /// <summary>
-    /// The Id of the species to which this breed belongs
-    /// </summary>
     public int SpeciesId { get; set; }
-
-    /// <summary>
-    /// Gets or sets the species information for the pet.
-    /// This property represents the species to which the pet belongs, 
-    /// and is encapsulated in a SpeciesViewModel object.
-    /// </summary>
     public SpeciesViewModel Species { get; set; }
 }
