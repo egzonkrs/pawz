05afaa5285b4bdb5dab365d919346ce855d7b1f6
Rigonm2 <rigonmej@gmail.com>
2024-09-05 00:10:10 +0300

    PAWZ-106-Added ErrorController and NotFound Page

--- src/Pawz.Web/Program.cs
+++ src/Pawz.Web/Program.cs
@@ -30,6 +30,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
