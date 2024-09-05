fd776f90a0e2a7ccf739584a2d1485862887f9aa
Daorsa Hyseni <79546481+Daxrsa@users.noreply.github.com>
2024-09-05 17:10:30 +0200

    PAWZ-101: image fix

--- src/Pawz.Web/Models/Pet/PetViewModel.cs
+++ src/Pawz.Web/Models/Pet/PetViewModel.cs
@@ -27,6 +27,7 @@ public class UserViewModel
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string? Address { get; set; }
+    public string? ImageUrl { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public ICollection<PetViewModel> Pets { get; set; }
     public ICollection<AdoptionRequest> AdoptionRequests { get; set; }
