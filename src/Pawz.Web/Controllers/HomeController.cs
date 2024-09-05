7dc1fe6f38cda68c4e0e160079d669b72393f723
Rigonm2 <rigonmej@gmail.com>
2024-09-05 11:33:39 +0300

    PAWZ-103-Added partials for PetLising

--- src/Pawz.Web/Controllers/HomeController.cs
+++ src/Pawz.Web/Controllers/HomeController.cs
@@ -1,22 +1,37 @@
+using AutoMapper;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Pawz.Application.Interfaces;
 using Pawz.Web.Models;
+using Pawz.Web.Models.Pet;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Pawz.Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IPetService _petService;
+    private readonly IMapper _mapper;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,
+        IPetService petService,
+        IMapper mapper)
     {
         _logger = logger;
+        _petService = petService;
+        _mapper = mapper;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
     {
-        return View();
+        var result = await _petService.GetAllPetsAsync(cancellationToken);
+        var petViewModels = _mapper.Map<IEnumerable<PetViewModel>>(result.Value);
+        return View(petViewModels);
     }
 
     public IActionResult Adopt()
