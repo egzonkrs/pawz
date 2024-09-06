fe7add3035c2737dd275e9fa37cc53b11255a6a4
ruhsulin <alinaspahiu.a@gmail.com>
2024-09-06 04:11:32 +0200

    PAWZ-111 bugfix: Added Surname as Claim to get user's last name.

--- src/Pawz.Application/Services/IdentityService.cs
+++ src/Pawz.Application/Services/IdentityService.cs
@@ -99,6 +99,7 @@ public sealed class IdentityService : IIdentityService
                      new Claim(ClaimTypes.NameIdentifier, user.Id),
                      new Claim(ClaimTypes.Email, user.Email),
                      new Claim(ClaimTypes.GivenName, user.FirstName),
+                     new Claim(ClaimTypes.Surname, user.LastName),
                      new Claim(ClaimTypes.Role, "User")
                 };
 
