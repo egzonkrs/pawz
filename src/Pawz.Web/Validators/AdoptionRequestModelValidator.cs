df3109253b96c6a5d9b09cfb1bfc9a959eb08f13
lauramuzaqi <muzaqilaura@gmail.com>
2024-09-12 21:23:21 +0200

    PAWZ-53 feat: Add new validations

--- src/Pawz.Web/Validators/AdoptionRequestModelValidator.cs
+++ src/Pawz.Web/Validators/AdoptionRequestModelValidator.cs
@@ -9,10 +9,12 @@ public class AdoptionRequestModelValidator : AbstractValidator<AdoptionRequestCr
     {
         RuleFor(x => x.CityId)
             .NotNull()
+            .NotEmpty().WithMessage("City is required.")
             .GreaterThan(0).WithMessage("City must be selected.");
 
         RuleFor(x => x.CountryId)
             .NotNull()
+            .NotEmpty().WithMessage("Country is required.")
             .GreaterThan(0).WithMessage("Country must be selected.");
 
         RuleFor(x => x.Address)
@@ -24,5 +26,37 @@ public class AdoptionRequestModelValidator : AbstractValidator<AdoptionRequestCr
             .NotNull()
             .NotEmpty().WithMessage("Postal code is required.")
             .MaximumLength(10).WithMessage("Postal code must be 10 characters or fewer.");
+
+        RuleFor(x => x.ContactNumber)
+            .NotNull()
+            .NotEmpty().WithMessage("Contact number is required.")
+            .Matches(@"^\+?\d{1,3}\s?\d{2}\s?\d{3}\s?\d{3}$").WithMessage("Contact number must be in the format +XXX XX XXX XXX.");
+
+        RuleFor(x => x.IsRentedProperty)
+            .NotNull()
+            .NotEmpty().WithMessage("This field is required.")
+            .IsInEnum().WithMessage("Please specify whether you live in a rented property.");
+
+        RuleFor(x => x.HasOutdoorSpace)
+            .NotNull()
+            .NotEmpty().WithMessage("This field is required.")
+            .IsInEnum().WithMessage("Please specify whether you have outdoor space.");
+
+        RuleFor(x => x.OutdoorSpaceDetails)
+            .MaximumLength(500).WithMessage("Outdoor space details must be 500 characters or fewer.")
+            .When(x => !string.IsNullOrEmpty(x.OutdoorSpaceDetails));
+
+        RuleFor(x => x.OwnsOtherPets)
+            .NotNull()
+            .NotEmpty().WithMessage("This field is required.")
+            .IsInEnum().WithMessage("Please specify whether you own other pets.");
+
+        RuleFor(x => x.OtherPetsDetails)
+            .MaximumLength(500).WithMessage("Other pets details must be 500 characters or fewer.")
+            .When(x => !string.IsNullOrEmpty(x.OtherPetsDetails));
+
+        RuleFor(x => x.Message)
+            .MaximumLength(1000).WithMessage("Message must be 1000 characters or fewer.")
+            .When(x => !string.IsNullOrEmpty(x.Message));
     }
 }
