9f4ad4d40ddb35c11c33973bea8e09e73995191d
ruhsulin <alinaspahiu.a@gmail.com>
2024-09-12 18:24:34 +0200

    PAWZ-74 feat: Added Mappings for application request.

--- src/Pawz.Web/Mappings/WebMappingProfiles.cs
+++ src/Pawz.Web/Mappings/WebMappingProfiles.cs
@@ -13,5 +13,6 @@ public class WebMappingProfiles : Profile
         CreateMap<Pet, AdoptionRequestCreateModel>();
 
         CreateMap<AdoptionRequestCreateModel, AdoptionRequestCreateRequest>();
+
     }
 }
