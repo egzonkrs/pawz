2a8ef29d56e924d5ae620ed4c32c816530404b0d
lauramuzaqi <muzaqilaura@gmail.com>
2024-09-12 19:21:00 +0200

    PAWZ-53 feat: Add new migration after adding new properties to AdoptionRequest entity

--- src/Pawz.Infrastructure/Migrations/20240912171325_AddNewPropertiesToAdoptionRequestEntity.Designer.cs
+++ src/Pawz.Infrastructure/Migrations/20240912171325_AddNewPropertiesToAdoptionRequestEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pawz.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using Pawz.Infrastructure.Data;
 namespace Pawz.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240912171325_AddNewPropertiesToAdoptionRequestEntity")]
+    partial class AddNewPropertiesToAdoptionRequestEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
