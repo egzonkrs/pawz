55117e441e5497218573484b20d6fe33d3f46c0d
lauramuzaqi <muzaqilaura@gmail.com>
2024-09-14 19:40:36 +0200

    PAWZ-53 fix: Changed YesNoEnum to boolean

--- src/Pawz.Web/Models/Pet/AdoptionRequestCreateModel.cs
+++ src/Pawz.Web/Models/Pet/AdoptionRequestCreateModel.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Pawz.Domain.Enums;
 using Pawz.Web.Models.City;
 using System.Collections.Generic;
 
@@ -60,12 +59,12 @@ public class AdoptionRequestCreateModel
     /// <summary>
     /// Gets or sets whether the adopter lives in a rented property.
     /// </summary>
-    public YesNoEnum IsRentedProperty { get; set; }
+    public bool IsRentedProperty { get; set; }
 
     /// <summary>
     /// Gets or sets whether the adopter has outdoor space.
     /// </summary>
-    public YesNoEnum HasOutdoorSpace { get; set; }
+    public bool HasOutdoorSpace { get; set; }
 
     /// <summary>
     /// Gets or sets the details about the adopter's outdoor space, if applicable.
@@ -75,7 +74,7 @@ public class AdoptionRequestCreateModel
     /// <summary>
     /// Gets or sets whether the adopter owns other pets.
     /// </summary>
-    public YesNoEnum OwnsOtherPets { get; set; }
+    public bool OwnsOtherPets { get; set; }
 
     /// <summary>
     /// Gets or sets the details about the adopter's other pets, if applicable.
