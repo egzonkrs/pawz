00ff27e871abd78088a2b4e829f2f0d99522907d
ruhsulin <alinaspahiu.a@gmail.com>
2024-09-17 04:07:51 +0200

    PAWZ-117 poc: Added dynamic data attributes to message button for owner, pet, and sender.

--- src/Pawz.Web/Models/Pet/PetViewModel.cs
+++ src/Pawz.Web/Models/Pet/PetViewModel.cs
@@ -24,6 +24,7 @@ public class PetViewModel
 
 public class UserViewModel
 {
+    public string Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string? Address { get; set; }
